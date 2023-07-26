d3dc02921b8ca5959d60b53ff8cebca3b5fb2c30
daph41 <daphinemoraa@gmail.com>
2023-07-26 04:55:33 -0700

    places

--- models/user.py
+++ models/user.py
@@ -26,4 +26,4 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
-        super().__init__(*args, **kwargs
+        super().__init__(*args, **kwargs)
